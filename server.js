fbd40c09682a051c618dd71f29fff0aa45b81021
Dmitry Sterlin <uglenpish@rambler.ru>
2023-05-26 16:11:39 +0300

    added package

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+}
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}))
